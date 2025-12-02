87698956ab1c20c75eb1c3c22b15fc006ebfdcc7
Su Shwe Sin Win <152246646+sushwesinwin@users.noreply.github.com>
2025-12-02 16:09:51 +0630

    feat: Add initial page components for event listing, individual event, event creation, and about page.

---
+++ app/events/[id]/page.js
@@ -0,0 +1,8 @@
+export default function EventPage() {
+  return (
+    <div>
+        <h1>EventPage</h1>
+    </div>
+  )
+}
+
---
+++ app/events/create.js/add.js
@@ -0,0 +1,7 @@
+export default function AddEventsPage() {
+  return (
+    <div>
+        <h1>Add Events</h1>
+    </div>
+  )
+}
